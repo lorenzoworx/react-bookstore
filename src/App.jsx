7c8b65930b74e5acd5b5ca2d9f984d056c214689
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-04-08 16:11:57 +0100

    Add react router to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import {
+  createBrowserRouter as createRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import BookList from './components/BookList';
+import Categories from './components/Categories';
+import RouteLayout from './components/RouteLayout';
 import './App.css';
+
+const routes = createRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RouteLayout />}>
+      <Route index element={<BookList />} />
+      <Route path="/categories" element={<Categories />} />
+    </Route>,
+  ),
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={routes} />
     </div>
   );
 }
